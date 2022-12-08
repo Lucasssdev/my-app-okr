6d8f83c2db7f3da42c827468ffc029b4aa847e7a
LucasLemos <lucaslfarah25@gmail.com>
2022-12-08 14:39:24 -0300

    load

--- assets/Styles/Home/index.js
+++ assets/Styles/Home/index.js
@@ -1,5 +1,11 @@
 import styled from "styled-components";
 
 export const Div = styled.div`
-color: red;
+color: brown;
+font-weight: 800;
+display: flex;
+justify-content: center;
+align-items: center;
+height: 100vh;
+width: 100vw;
 `
